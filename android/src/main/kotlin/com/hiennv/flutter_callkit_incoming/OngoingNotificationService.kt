fb5035792f708948e588c9bf32cf7aafb08b095e
Andrew Steven <andrew.steven23@hotmail.com>
2025-05-13 16:01:22 +0100

    Removed the restricted foreground service type which is causing the app to crash when attempting to make calls on production.

--- android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/OngoingNotificationService.kt
+++ android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/OngoingNotificationService.kt
@@ -169,17 +169,7 @@ class OngoingNotificationService : Service() {
         }
         notificationBuilder.setOngoing(true)
         val notification = notificationBuilder.build()
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            startForeground(
-                onGoingNotificationId,
-                notification,
-                ServiceInfo.FOREGROUND_SERVICE_TYPE_PHONE_CALL
-            )
-        } else {
-            startForeground(onGoingNotificationId, notification)
-        }
-
+        startForeground(onGoingNotificationId, notification)
     }
 
     private fun getHangupPendingIntent(notificationId: Int, data: Bundle): PendingIntent {
