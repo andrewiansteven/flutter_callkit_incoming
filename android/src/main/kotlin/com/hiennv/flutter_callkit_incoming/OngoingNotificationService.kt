d0b1498802048e97bd4e574ab64919c45d8ab80d
Andrew Steven <andrew.steven23@hotmail.com>
2025-05-13 17:20:07 +0100

    Additional permissions for Android 14+ (API level 34+)

--- android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/OngoingNotificationService.kt
+++ android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/OngoingNotificationService.kt
@@ -169,7 +169,25 @@ class OngoingNotificationService : Service() {
         }
         notificationBuilder.setOngoing(true)
         val notification = notificationBuilder.build()
-        startForeground(onGoingNotificationId, notification)
+
+        val typeCall = data.getInt(CallkitConstants.EXTRA_CALLKIT_TYPE, -1)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            var serviceType = ServiceInfo.FOREGROUND_SERVICE_TYPE_PHONE_CALL
+            if (typeCall > 0){
+                serviceType = serviceType or ServiceInfo.FOREGROUND_SERVICE_TYPE_MICROPHONE or ServiceInfo.FOREGROUND_SERVICE_TYPE_CAMERA
+            }
+            else {
+                serviceType = serviceType or ServiceInfo.FOREGROUND_SERVICE_TYPE_MICROPHONE
+            }
+            startForeground(
+                onGoingNotificationId,
+                notification,
+                ServiceInfo.FOREGROUND_SERVICE_TYPE_PHONE_CALL
+                        serviceType
+            )
+        } else {
+            startForeground(onGoingNotificationId, notification)
+        }
     }
 
     private fun getHangupPendingIntent(notificationId: Int, data: Bundle): PendingIntent {
